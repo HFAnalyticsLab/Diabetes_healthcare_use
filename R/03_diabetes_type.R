f696c495998e3c8c7c42dfb71c843f2536c76b5c
fiona-grimm <fiona.grimm.zh@gmail.com>
2019-09-23 16:04:51 +0100

    Replaced OHA with NIGLD

--- R/03_diabetes_type.R
+++ R/03_diabetes_type.R
@@ -60,6 +60,7 @@ diabetes_list_bypat <- clinical_diabetes %>%
 
 # Join Read code counts and list
 diabetes_bypat <- diabetes_count_bypat %>% 
+  ungroup() %>% 
   left_join(diabetes_list_bypat, by = 'patid')
 
 # Assigning a diabetes type (step 1: unambiguous diagnoses) --------------
@@ -73,8 +74,10 @@ diabetes_bypat <- diabetes_bypat %>%
 
 
 # Step 2: Using prescription information from before the study start ----
-# Assign type 2 if patients have been on more than one type of OHA in the past
+# Assign type 2 if patients have been on more than one type of NIGLD in the past
 # (not necessarily at the same time)
+
+# categorise into insulin only, non-insulin glucose lowering drug (NIGLD, OHA and GLP-1 injectables) only, both insulin and NIGLD
 
 therapy_diabetes <- extract_therapy %>% 
   select(patid, eventdate, prodcode) %>%
@@ -83,40 +86,60 @@ therapy_diabetes <- extract_therapy %>%
 
 # NB will exclude acarbose, as it is technically not an OHA
 prescriptions_bypat <- therapy_diabetes %>% 
-  filter(drug_type != 'Acarbose' & drug_type != 'Insulin') %>% 
+  filter(drug_type != 'Acarbose') %>% 
   group_by(patid, drug_type) %>% 
   summarise(count = n()) 
 
-# Combination OHAs need to counted in both individual drug type categories
+# Convert to binary 
+# Combination NIGLD need to counted in both individual drug type categories
 therapy_bypat <-  prescriptions_bypat %>% 
+  ungroup() %>% 
   mutate(count = ifelse(count >= 1, 1L, 0)) %>%  
   spread(key = 'drug_type', value = 'count') %>% 
-  mutate(`GLP-1` = ifelse(!is.na(`Insulin/glp-1`) & `Insulin/glp-1` == 1, 1L, `GLP-1`),
+  mutate(Insulin = ifelse(!is.na(`Insulin/glp-1`) & `Insulin/glp-1` == 1, 1L, Insulin),
+         `GLP-1` = ifelse(!is.na(`Insulin/glp-1`) & `Insulin/glp-1` == 1, 1L, `GLP-1`),
          Metformin = ifelse(!is.na(`Metformin/Glitazone`) & `Metformin/Glitazone` == 1, 1L, Metformin),
          Metformin = ifelse(!is.na(`Metformin/SGLT2-i`) & `Metformin/SGLT2-i` == 1, 1L, Metformin),
          Metformin = ifelse(!is.na(`Metformin/DPP-4i`) & `Metformin/DPP-4i` == 1, 1L, Metformin),
          Glitazone = ifelse(!is.na(`Metformin/Glitazone`) & `Metformin/Glitazone` == 1, 1L, Glitazone),
          `SGLT2-i` = ifelse(!is.na(`Metformin/SGLT2-i`) & `Metformin/SGLT2-i` == 1, 1L, `SGLT2-i`),
          `DPP-4i` = ifelse(!is.na(`Metformin/DPP-4i`) & `Metformin/DPP-4i` == 1, 1L, `DPP-4i`)) %>% 
+  mutate_all(funs(replace_na(., 0))) %>% 
   select(-`Insulin/glp-1`, -`Metformin/Glitazone`, -`Metformin/SGLT2-i`, -`Metformin/DPP-4i`)
 
 therapy_bypat <- therapy_bypat %>% 
-  mutate(OHA_multiple = ifelse(sum(`DPP-4i`, Glitazone, `GLP-1`, Meglitinide, Metformin, `SGLT2-i`, Sulfonylurea, na.rm = TRUE) >= 2, 1, 0))
+  mutate(NIGLD_count = `DPP-4i` + Glitazone + `GLP-1` + Meglitinide + Metformin + `SGLT2-i` + Sulfonylurea, 
+         NIGLD_multiple = ifelse(NIGLD_count>= 2, 1, 0))
 
-therapy_bypat %>% 
-  tabyl(OHA_multiple)
 
-# Assign type 2 based on prescriptions
+# Step 2. Assign type 2 based on prescriptions: if type unknown and ever been on more than one NIGLD 
 diabetes_bypat <- diabetes_bypat %>% 
-  left_join(therapy_bypat[, c('patid', 'OHA_multiple')], by = 'patid') %>% 
-  mutate(diabetes_type = ifelse(is.na(diabetes_type) & OHA_multiple == 1, 'type2', diabetes_type))
+  left_join(therapy_bypat[, c('patid', 'NIGLD_multiple', 'Insulin')], by = 'patid') 
 
+diabetes_bypat %>% 
+  tabyl(diabetes_type, NIGLD_multiple) %>% 
+  adorn_title()
 
-# Step 3: Join with study population, all patients left to be assigned will be diabetes type 'unknown' ----
+diabetes_bypat <- diabetes_bypat %>% 
+  mutate(diabetes_type = ifelse(!is.na(NIGLD_multiple) & NIGLD_multiple == 1, 'type2', diabetes_type))
+
+# Step 3. Convert type 1 to type 2 if 
+# never been prescribed insulin
+# ever been prescribed more than one NIGLD
+diabetes_bypat %>% 
+  tabyl(diabetes_type, Insulin) %>% 
+  adorn_title()
+
+diabetes_bypat <- diabetes_bypat %>% 
+  mutate(diabetes_type = ifelse(!is.na(diabetes_type) & diabetes_type == 'type1' & (is.na(Insulin) | Insulin == 0), 'type2', diabetes_type))
+
+# Step 4: Join with study population, all patients left to be assigned will be diabetes type 'unknown' ----
 diabetes_bypat <- diabetes_bypat %>% 
   right_join(patients[,'patid'], by = 'patid') %>% 
   mutate(diabetes_type = ifelse(is.na(diabetes_type), 'unknown', diabetes_type))
 
+diabetes_bypat %>% 
+  tabyl(diabetes_type)
 
 # Save processed data -----------------------------------------------------
 
