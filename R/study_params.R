1b8d7cbff896e23431bdfad21320a722fb3ffd8a
fiona-grimm <fiona.grimm.zh@gmail.com>
2019-10-02 09:37:06 +0100

    Initial commit

---
+++ R/study_params.R
@@ -0,0 +1,9 @@
+# Define study parameters -------------------------------------------------
+
+# Year 1 and 2 to quantify utilisation and other covariates
+study_start <- ymd('2015-12-01')
+study_end <- ymd('2017-11-30')
+
+# Year 3 for health outcomes
+followup_start <- ymd('2017-12-01')
+followup_end <-  ymd('2018-11-30')
