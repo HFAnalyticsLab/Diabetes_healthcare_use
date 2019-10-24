db17f2aeb860d3bd5f3f95d4d38424c0fc6beff2
fiona-grimm <fiona.grimm.zh@gmail.com>
2019-10-24 12:00:00 +0100

    Replaced file paths with variables, changed definition of time since diagnosis variables

--- R/08_recentlydiagnosed.R
+++ R/08_recentlydiagnosed.R
@@ -2,7 +2,6 @@
 # Project: Diabetes outpatient care
 # Purpose: Determine wether first diagnosis was within last x years
 # Author: Fiona Grimm
-# Date: 12/09/2019
 # =======================================================
 # TO DO: figure out whether/how practice UTS date needs to be taken into account
 
@@ -12,27 +11,24 @@ library(tidylog)
 library(janitor)
 
 # Source file paths: Rds_path
-source('R_FG/file_paths.R')
+source('R/file_paths.R')
 
-# Define study parameters -------------------------------------------------
-
-# Year 1 and 2 to quantify utilisation and other covariates
-study_start <- ymd('2015-12-01')
-study_end <- ymd('2017-11-30')
+# Source study parameters 
+source('R/study_params.R')
 
 # Import data -----------------------------------------
 
 # Patiend IDs
-patients <- readRDS('processed_data/patients.Rds')
+patients <- readRDS(str_c(processed_RDS_path, 'patients.Rds'))
 
 # Diabetes code list
 diabetes_codes <- read_csv(str_c(code_list_path, 'Appendix1_diabetes_diagnosis.csv'))
 
 # Clinical data
-extract_clinical <- readRDS('raw_data/Extract_clinical.Rds')
+extract_clinical <- readRDS(str_c(raw_RDS_path, 'Extract_clinical.Rds'))
 
 # Diabetes type
-diabetes_bypat <- readRDS('processed_data/patients_diabetes.rds')
+diabetes_bypat <- readRDS(str_c(processed_RDS_path, 'patients_diabetes.rds'))
 
 
 # Extract diabetes Read codes ---------------------------------------------
@@ -41,7 +37,6 @@ diabetes_bypat <- readRDS('processed_data/patients_diabetes.rds')
   
 clinical_diabetes <- extract_clinical %>% 
   filter(medcode %in% diabetes_codes$medcode & eventdate < study_start) 
-
 
 clinical_diabetes_first <-  clinical_diabetes %>% 
     arrange(patid, eventdate) %>%
@@ -61,21 +56,36 @@ clinical_diabetes_first %>%
   ggplot(aes(x = age_at_diagnosis, group = diabetes_type, color = diabetes_type)) +
   geom_density()
 
-# Constructing binary variables for 
-# First diagnosis within six months before study start
-# First diagnosis within two years before study start
-# First diagnosis within five years before study start
+# Similar to what is reported in the National Diabetes Audit, will construct binary variables for 
+# First diagnosis within less than 1 year before study start
+# First diagnosis within 1-4 years before study start
+# First diagnosis within 5-9 years before study start
 
+# NB this is incredibly slow - think about finding faster method
 clinical_diabetes_first <-  clinical_diabetes_first %>% 
   select(-age_at_diagnosis) %>% 
-  mutate(diag_6m = ifelse(eventdate %within% interval(study_start - months(6), study_start-1), 1, 0),
-         diag_2y = ifelse(eventdate %within% interval(study_start - years(2), study_start-1), 1, 0),
-         diag_5y = ifelse(eventdate %within% interval(study_start - years(5), study_start-1), 1, 0))
+  mutate(diag_less_than_1y = ifelse(eventdate %within% interval(study_start - years(1) , study_start - 1), 1, 0),
+         diag_1_to_4y = ifelse(eventdate %within% interval(study_start - years(4), study_start - years(1) - 1), 1, 0),
+         diag_5_to_9y = ifelse(eventdate %within% interval(study_start - years(9), study_start - years(4) - 1), 1, 0))
 
 clinical_diabetes_first %>% 
-  tabyl(diag_6m, diag_2y) %>% 
+  tabyl(diag_less_than_1y, diag_1_to_4y) %>% 
   adorn_title()
+
+clinical_diabetes_first %>% 
+  tabyl(diag_less_than_1y, diag_5_to_9y) %>% 
+  adorn_title()
+
+clinical_diabetes_first %>% 
+  tabyl(diag_1_to_4y, diag_5_to_9y) %>% 
+  adorn_title()
+
+clinical_diabetes_first <-  clinical_diabetes_first %>% 
+  mutate(time_since_diagnosis = case_when(diag_less_than_1y == 1 ~ 'less than 1 year',
+                                          diag_1_to_4y == 1 ~ '1 to 4 years',
+                                          diag_5_to_9y == 1 ~ '5 to 9 years',
+                                          diag_less_than_1y ==  0 & diag_1_to_4y == 0 & diag_5_to_9y == 0 ~ 'more than 9 years'))
 
 # Saving processed files --------------------------------------------------
 
-saveRDS(clinical_diabetes_first, 'processed_data/patients_diabetes_firstdiagnosis.rds')
+saveRDS(clinical_diabetes_first, str_c(processed_RDS_path, 'patients_diabetes_firstdiagnosis.rds'))
