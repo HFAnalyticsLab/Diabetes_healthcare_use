18944b95d4de801e52f02e033a29ea7f2d3321cb
fiona-grimm <fiona.grimm.zh@gmail.com>
2019-10-24 12:01:13 +0100

    Normlise count by time in study, create summary tables for number of prescriptions

--- R/13_prescriptions.R
+++ R/13_prescriptions.R
@@ -44,8 +44,56 @@ prescripts_bypat <-  prescripts %>%
 prescripts_bypat <-  prescripts_bypat %>% 
   right_join(patients[, c('patid')], by = 'patid') 
 
+# Normalising by time spent in study --------------------------------------
+
+prescripts_bypat_norm <- prescripts_bypat %>% 
+  right_join(patients[, c('patid', 'tod', 'ONS_dod', 'resquality', 'diabetes_type')], by = 'patid') %>% 
+  filter(resquality == 1 & diabetes_type %in% c('type1', 'type2')) %>% 
+  select(-resquality) %>% 
+  mutate_if(is.numeric, ~replace_na(.x, 0))
+
+
+prescripts_bypat_norm <-  prescripts_bypat_norm %>% 
+  group_by(patid) %>% 
+  mutate(censoring_date = min(tod, ONS_dod, study_end, na.rm = TRUE),
+         years_in_study = round(as.numeric(censoring_date - study_start) / 365, 2),
+         years_in_study = ifelse(years_in_study == 0, 0.01, years_in_study))
+
+prescripts_bypat_norm <- prescripts_bypat_norm %>% 
+  ungroup() %>% 
+  select(-tod, -ONS_dod, -censoring_date) %>% 
+  mutate(n_prescripts_per_year = round(n_prescripts / years_in_study, 1),
+         n_prescripts_diabetes_per_year = round(n_prescripts_diabetes / years_in_study, 1))
+
 # Saving processed files --------------------------------------------------
 
-saveRDS(prescripts_bypat, 'processed_data/patients_prescriptions.rds')
+saveRDS(prescripts_bypat_norm, str_c(processed_RDS_path, 'patients_prescriptions.rds'))
 
+
+# Create summary tables -------------------------------------------------
+# Means
+
+all_prescr_means <- prescripts_bypat_norm %>% 
+  group_by(diabetes_type) %>% 
+  summarise(n = n(),
+            mean_prescr_count = round(mean(n_prescripts), 1),
+            mean_prescr_count_per_year = round(mean(n_prescripts_per_year), 1),
+            mean_prescr_diabetes_count = round(mean(n_prescripts_diabetes), 1),
+            mean_prescr_diabetes_count_per_year = round(mean(n_prescripts_diabetes_per_year), 1)) %>% 
+  mutate(subgroup = 'all patients')
+
+not_censored_prescr_means <- prescripts_bypat_norm %>% 
+  group_by(diabetes_type) %>% 
+  filter(years_in_study == 2) %>% 
+  summarise(n = n(),
+            mean_prescr_count = round(mean(n_prescripts), 1),
+            mean_prescr_count_per_year = round(mean(n_prescripts_per_year), 1),
+            mean_prescr_diabetes_count = round(mean(n_prescripts_diabetes), 1),
+            mean_prescr_diabetes_count_per_year = round(mean(n_prescripts_diabetes_per_year), 1)) %>% 
+  mutate(subgroup = 'did not transfer out')
+
+prescr_means <- all_prescr_means %>% 
+  bind_rows(not_censored_prescr_means)
+
+write_csv(prescr_means, str_c(summary_stats_path, 'table2/Prescriptions_means.csv'))
 
