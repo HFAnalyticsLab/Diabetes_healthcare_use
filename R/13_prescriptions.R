31cae89a6e86d0e8bdb22c738ec91fc0cbf56dd8
fiona-grimm <fiona.grimm.zh@gmail.com>
2019-11-25 16:40:32 +0000

    Implemented new cohort definitions and names, and new follow up time definition for CPRD data

--- R/13_prescriptions.R
+++ R/13_prescriptions.R
@@ -19,7 +19,6 @@ source('R/study_params.R')
 
 patients <- readRDS(str_c(processed_RDS_path, 'patients_clinical_combined.Rds'))
 
-
 # Diabetes medication code list
 medication_codes <- read_csv(str_c(code_list_path, 'Appendix5_antidiabetics.csv'))
 
@@ -32,6 +31,18 @@ extract_therapy <- readRDS(str_c(raw_RDS_path, 'Extract_therapy.Rds'))
 prescripts <- extract_therapy %>% 
   filter(eventdate %within% interval(study_start, study_end)) %>% 
   mutate(diabetes_pres = ifelse(prodcode %in% medication_codes$product_code, 1, 0))
+
+
+# Check: how many prescriptions are for patients in the step 2 and step 3 populations?
+prescripts %>% 
+  left_join(patients[, c('patid', 'cohort_step2')], by = 'patid') %>% 
+  filter(cohort_step2 == 1) %>% 
+  nrow()
+
+prescripts %>% 
+  left_join(patients[, c('patid', 'cohort_step3')], by = 'patid') %>% 
+  filter(cohort_step3 == 1) %>% 
+  nrow()
 
 # Counting prescriptions per patient ---------------------------------------
 
@@ -47,23 +58,14 @@ prescripts_bypat <-  prescripts_bypat %>%
 # Normalising by time spent in study --------------------------------------
 
 prescripts_bypat_norm <- prescripts_bypat %>% 
-  right_join(patients[, c('patid', 'tod', 'ONS_dod', 'resquality', 'diabetes_type')], by = 'patid') %>% 
-  filter(resquality == 1 & diabetes_type %in% c('type1', 'type2')) %>% 
-  select(-resquality) %>% 
+  right_join(patients[, c('patid', 'tod', 'ONS_dod', 'cohort_step2', 'cohort_step3', 'diabetes_type', 'years_in_study_cprd')], by = 'patid') %>% 
+  filter(cohort_step2 == 1 & diabetes_type %in% c('type1', 'type2')) %>% 
+  select(-cohort_step2) %>% 
   mutate_if(is.numeric, ~replace_na(.x, 0))
 
-
-prescripts_bypat_norm <-  prescripts_bypat_norm %>% 
-  group_by(patid) %>% 
-  mutate(censoring_date = min(tod, ONS_dod, study_end, na.rm = TRUE),
-         years_in_study = round(as.numeric(censoring_date - study_start) / 365, 2),
-         years_in_study = ifelse(years_in_study == 0, 0.01, years_in_study))
-
 prescripts_bypat_norm <- prescripts_bypat_norm %>% 
-  ungroup() %>% 
-  select(-tod, -ONS_dod, -censoring_date) %>% 
-  mutate(n_prescripts_per_year = round(n_prescripts / years_in_study, 1),
-         n_prescripts_diabetes_per_year = round(n_prescripts_diabetes / years_in_study, 1))
+  mutate(n_prescripts_per_year = round(n_prescripts / years_in_study_cprd, 1),
+         n_prescripts_diabetes_per_year = round(n_prescripts_diabetes / years_in_study_cprd, 1))
 
 # Saving processed files --------------------------------------------------
 
@@ -84,7 +86,7 @@ all_prescr_means <- prescripts_bypat_norm %>%
 
 not_censored_prescr_means <- prescripts_bypat_norm %>% 
   group_by(diabetes_type) %>% 
-  filter(years_in_study == 2) %>% 
+  filter(years_in_study_cprd == 2) %>% 
   summarise(n = n(),
             mean_prescr_count = round(mean(n_prescripts), 1),
             mean_prescr_count_per_year = round(mean(n_prescripts_per_year), 1),
