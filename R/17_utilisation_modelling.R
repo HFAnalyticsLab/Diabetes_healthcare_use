55a989e26516e8c53b1597cd809c51afb3b0bba9
fiona-grimm <fiona.grimm.zh@gmail.com>
2020-03-12 16:40:36 +0000

    Remove redundant code

--- R/17_utilisation_modelling.R
+++ R/17_utilisation_modelling.R
@@ -1,6 +1,6 @@
 # =======================================================
 # Project: Diabetes outpatient care
-# Purpose: Explanatory statistical models for healthcare utilisation for patients with T2DM
+# Purpose: Explanatory statistical models for healthcare utilisation (by type)
 # Author: Fiona Grimm
 # =======================================================
 
@@ -16,31 +16,15 @@ library(ggrepel)
 # Source file paths: Rds_path
 source('R/file_paths.R')
 
-# Source study parameters 
-#source('R/study_params.R')
-
 # Source graph parameters 
-source('R/plotting_params.R')
+source('R/study_params.R')
 
 # Functions ---------------------------------------------------------------
 
-# ggplot theme
-utilisation_theme <- theme_classic() +
-  theme(legend.position = 'top',
-        legend.text = element_text(color = THF_dark_grey),
-        legend.title = element_text(color = THF_dark_grey),
-        legend.justification = c(1,0),
-        legend.key.size = unit(3, 'mm'),
-        legend.spacing.x = unit(1 ,'mm'),
-        axis.line = element_line(size = 0.25),
-        axis.ticks = element_line(size = 0.25)) +
-  THF_theme
-
 # a function to summarise the coefficients of negative binomial regression models,
 # including confidence intervals
-summariseNbModel <- function(model, type = c('nb', 'cox', 'binomial')){
+summariseNbModel <- function(model){
   
-  if(type == 'nb'){
     table <- broom::tidy(model, exponentiate = TRUE) %>% 
       cbind(exp(confint(model))) 
     
@@ -51,44 +35,19 @@ summariseNbModel <- function(model, type = c('nb', 'cox', 'binomial')){
       select(Coefficient, IRR, CI_lower, CI_upper, everything()) %>% 
       mutate_at(vars(IRR, z.value, CI_lower, CI_upper), round, 3)
     
-    return(table)} else if(type = 'cox'){
-      table <- broom::tidy(model, exponentiate = TRUE) 
-      
-      rownames(table) <- NULL
-      colnames(table) <- c('Coefficient', 'HR', 'Std.Error', 'z.value', 'p.value', 'CI_lower', 'CI_upper')
-      
-      table <- table %>% 
-        select(Coefficient, HR, CI_lower, CI_upper, everything()) %>% 
-        mutate_at(vars(HR, z.value, CI_lower, CI_upper), round, 3)
-      
-      return(table)
-    }
+    return(table)
 }
-
 
 # Import data -------------------------------------------------------------
 
 # Modelling data was created in script 16
-model_data_T1D <- readRDS(str_c(processed_RDS_path, 'Utilisation_modelling_data_T1D.Rds'))
 model_data_T2D <- readRDS(str_c(processed_RDS_path, 'Utilisation_modelling_data_T2D.Rds'))
 
-# Prevalence of conditions
-mm_prev <- read_csv(str_c(summary_stats_path, 'multimorbidity/MM_prevalence.csv'))
-
-# Keep 20 most prevalent conditions
-mm_prev_T1D <- mm_prev %>% 
-  filter(diabetes_type ==  'type1' & !is.element(condition, c('DEPANX', 'DEP', 'ANX', 'ANXr'))) %>% 
-  arrange(desc(prevalence)) %>% 
-  head(20)
-
-mm_prev_T2D <- mm_prev %>% 
-  filter(diabetes_type ==  'type2' & !is.element(condition, c('DEPANX', 'DEP', 'ANX', 'ANXr'))) %>% 
-  arrange(desc(prevalence)) %>% 
-  head(20)
-
+#####
 # Modelling utilisation of people with T2DM: negative binomial models -------------------------------------
+#####
 
-## PRIMARY CARE CONSULTATIONS
+# PRIMARY CARE CONSULTATIONS ----------------------------------------------
 # Base model
 m_T2D_primcare_base <- glm.nb(consult_primarycare ~ female + age_5ybin_centered + offset(log(years_in_study_cprd)), 
                                     data = model_data_T2D)
