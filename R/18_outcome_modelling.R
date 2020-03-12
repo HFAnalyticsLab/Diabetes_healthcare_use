786bea00d813ca1219d1beb3aad4e959700730d2
fiona-grimm <fiona.grimm.zh@gmail.com>
2020-03-12 16:41:32 +0000

    Remove redundant code, implement separate models for DEPANXr and PNC

--- R/18_outcome_modelling.R
+++ R/18_outcome_modelling.R
@@ -1,7 +1,7 @@
 # =======================================================
 # Project: Diabetes outpatient care
-# Purpose: Cox model to examine effect of long-term conditions on outcomes for patients with T2DM
-# (emergeny hospital admission, all-cause mortality)
+# Purpose: Cox model to examine effect of CMD and PNC on outcomes 
+# for people with T2DM (emergency hospital admission, all-cause mortality)
 # Author: Fiona Grimm
 # =======================================================
 
@@ -48,45 +48,22 @@ patients <- readRDS(str_c(processed_RDS_path, 'patients_clinical_combined.Rds'))
 # Hospital admissions
 hesapc_admissions <- readRDS(str_c(processed_RDS_path, 'admissions.rds'))
 
-# Prevalence of conditions
-mm_prev <- read_csv(str_c(summary_stats_path, 'multimorbidity/MM_prevalence.csv'))
-
-# Keep 20 most prevalent conditions apart from CMD
-mm_prev_T2D <- mm_prev %>% 
-  filter(diabetes_type ==  'type2' & !is.element(condition, c('DEPANX', 'DEP', 'ANX', 'ANXr'))) %>% 
-  arrange(desc(prevalence)) %>% 
-  head(20)
-
-# Prepare mortality model data (death within study period) ---------------------------
-
-model_data <- patients %>% 
-  filter(cohort_step2 == 1) %>% 
-  mutate(mental_disorder = 'None',
-         mental_disorder = ifelse(DEPANXr == 1, 'CMD', mental_disorder),
-         mental_disorder = ifelse(SCZ == 1, 'SMD', mental_disorder),
-         DEPANXr_PNC = case_when(DEPANXr == 0 & PNC == 0 ~ 'none',
-                                 DEPANXr == 1 & PNC == 0 ~ 'DEPANXr',
-                                 DEPANXr == 0 & PNC == 1 ~ 'PNC',
-                                 DEPANXr == 1 & PNC == 1 ~ 'DEPANXr/PNC'),
-         DEPANXr_PNC = fct_relevel(DEPANXr_PNC, 'none', 'DEPANXr', 'PNC', 'DEPANXr/PNC'))
-
-model_data %>%  tabyl(DEPANXr_PNC)
+# Prepare T2D modelling data for all-cause mortality (death within study period) ----
 
 # Censoring time 
-
 # Type of even/censoring
 # 1 = censored (study end)
 # 2 = died
-model_data <-  model_data %>% 
+model_data_mortality <- patients %>% 
+  filter(cohort_step2 == 1) %>% 
   group_by(patid) %>% 
   mutate(earliest_eventdate = min(ONS_dod, study_end, na.rm = TRUE),
          time_to_event = as.numeric(earliest_eventdate - study_start, 'days'),
          status = 1,
-         status = ifelse(!is.na(ONS_dod) & earliest_eventdate == ONS_dod, 2, status),
-         death = ifelse(!is.na(ONS_dod) & earliest_eventdate == ONS_dod, 1, 0)) %>% 
+         status = ifelse(!is.na(ONS_dod) & earliest_eventdate == ONS_dod, 2, status)) %>% 
   ungroup()
 
-model_data <-  model_data %>% 
+model_data_mortality <-  model_data_mortality %>% 
   filter(diabetes_type == 'type2') %>% 
   mutate(diabetes_type = fct_drop(diabetes_type),
          female = ifelse(gender == '1', 0, 1),
@@ -94,52 +71,107 @@ model_data <-  model_data %>%
          imd_quintile_numeric = as.numeric(gsub(' .*', '', imd_quintile)),
          smoker = ifelse(smoking_status == 'smoker', 1,0),
          newly_diagnosed_1y = ifelse(time_since_diagnosis == 'less than 1 year', 1, 0),
-         HbA1C_control_bad = ifelse(HbA1C_control == 'bad', 1, 0))
+         age_centered = startage_study - round(mean(startage_study)),
+         age_5ybin_centered = age_centered / 5,
+         mm_count_excl_DEPANXr = rowSums(.[comorbidities[comorbidities != 'DEPANXr']]),
+         mm_count_excl_PNC = rowSums(.[comorbidities[comorbidities != 'PNC']]))
 
-# Split by diabetes type
-model_data_T2D <- model_data %>% 
-  mutate(age_centered = startage_study - round(mean(startage_study)),
-         age_5ybin_centered = age_centered / 5)
+# Bin survival time into weeks for kaplan-meier plots
+model_data_mortality <-  model_data_mortality %>% 
+  mutate(time_to_event_weeks = ceiling(time_to_event/7),
+         time_to_event_weeks = ifelse(time_to_event_weeks == 0, 1, time_to_event_weeks),
+         time_to_event_4weeks = time_to_event_weeks + 4 - time_to_event_weeks %% 4)
 
+# Cumulative incidence of death, split by DEPANX
 
-# mortality Cox PH ------------------------------------------------------------------
-
-ggsurvplot(fit = survfit(Surv(time = time_to_event, event = status) ~ imd_quintile,
-                         data = model_data_T2D), 
-           xlab = 'Days', ylab = 'Cumulative incidence', facet.by = 'DEPANXr',
+ggsurvplot(fit = survfit(Surv(time = time_to_event, event = status) ~ DEPANXr,
+                         data = model_data_mortality), 
+           xlab = 'Days', ylab = 'Cumulative incidence', 
            censor = FALSE, conf.int = TRUE, fun = 'event')
 
-T2D_coxmodel_base <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric  
-                        + HbA1C_control + smoker , data = model_data_T2D)
+ggsurvplot(fit = survfit(Surv(time = time_to_event, event = status) ~ PNC,
+                         data = model_data_mortality), 
+           xlab = 'Days', ylab = 'Cumulative incidence', 
+           censor = FALSE, conf.int = TRUE, fun = 'event')
 
-T2D_coxmodel_base_summary <- summariseCoxModel(model = T2D_coxmodel_base)
-write_csv(T2D_coxmodel_base_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_basemodel.csv'))
-
-
-T2D_coxmodel_1 <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric + DEPANXr 
-                     + HbA1C_control + physical_mm_count , data = model_data_T2D)
-summary(T2D_coxmodel_1)
-extractAIC(T2D_coxmodel_1)
-
-T2D_coxmodel_2 <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric  
-                     + HbA1C_control + smoker + DEPANXr + physical_mm_count , data = model_data_T2D) # slightly better
-summary(T2D_coxmodel_2)
-extractAIC(T2D_coxmodel_2)
-
-T2D_coxmodel_2_summary <- summariseCoxModel(model = T2D_coxmodel_2)
-write_csv(T2D_coxmodel_2_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_DEPANXr.csv'))
+ggsurvplot(fit = survfit(Surv(time = time_to_event, event = status) ~ mm_cat,
+                         data = model_data_mortality), 
+           xlab = 'Days', ylab = 'Cumulative incidence', 
+           censor = FALSE, conf.int = TRUE, fun = 'event')
 
 
-T2D_coxmodel_4 <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric + DEPANXr_PNC 
-                        + HbA1C_control + physical_mm_count + smoker, data = model_data_T2D) # best
+# T2D Mortality Cox PH ------------------------------------------------------------------
 
-T2D_coxmodel_4_summary <- summariseCoxModel(model = T2D_coxmodel_4)
-write_csv(T2D_coxmodel_4_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_DEPANXr_PNC.csv'))
+T2D_mortality_coxmodel_base <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric  
+                        + HbA1C_control + smoker + newly_diagnosed_1y, data = model_data_mortality)
 
-extractAIC(T2D_coxmodel_4)
+T2D_mortality_coxmodel_base_summary <- summariseCoxModel(model = T2D_mortality_coxmodel_base)
+write_csv(T2D_mortality_coxmodel_base_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_basemodel.csv'))
+
+# DEPANXr
+T2D_mortality_coxmodel_DEPANXr <- update(T2D_mortality_coxmodel_base, . ~ . + mm_count_excl_DEPANXr + DEPANXr )
+summary(T2D_mortality_coxmodel_DEPANXr)
+extractAIC(T2D_mortality_coxmodel_DEPANXr)
+
+T2D_mortality_coxmodel_DEPANXr_summary <- summariseCoxModel(model = T2D_mortality_coxmodel_DEPANXr)
+write_csv(T2D_mortality_coxmodel_DEPANXr_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_DEPANXr.csv'))
+
+# PNC
+T2D_mortality_coxmodel_PNC <- update(T2D_mortality_coxmodel_base, . ~ . + mm_count_excl_PNC + PNC )
+summary(T2D_mortality_coxmodel_PNC)
+extractAIC(T2D_mortality_coxmodel_PNC)
+
+T2D_mortality_coxmodel_PNC_summary <- summariseCoxModel(model = T2D_mortality_coxmodel_PNC)
+write_csv(T2D_mortality_coxmodel_PNC_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_PNC.csv'))
 
 
-# Outcome: emergency admission --------------------------------------------
+# Model all conditions invididually 
+T2D_coxmodel_allmm <- update(T2D_mortality_coxmodel_base, as.formula(str_c('. ~ . + ', str_c(comorbidities, collapse = ' + '))))
+
+T2D_coxmodel_allmm_summary <- summariseCoxModel(model = T2D_coxmodel_allmm)
+write_csv(T2D_coxmodel_allmm_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_Mortality_allmm.csv'))
+
+# Kaplan meier plots
+
+T2DM_mortality_survplot_DEPANXr <- ggsurvplot(fit = survfit(Surv(time = time_to_event_weeks, event = status) ~ DEPANXr,
+                         data = model_data_mortality), 
+           xlab = 'Weeks', ylab = 'Cumulative incidence',
+           censor = FALSE,  fun = 'event', ggtheme = theme_bw(), size = 0.5,
+           palette = THF_discrete_colour_order[1:2], legend = c(0.2,0.85),
+           legend.labs = c('Without CMD', 'With CMD'), title = 'All-cause mortality') 
+
+T2DM_mortality_survplot_DEPANXr <- T2DM_mortality_survplot_DEPANXr$plot +
+  theme(legend.title = element_blank())
+
+ggsave(str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_Mortality_DEPANXr.png'), 
+       T2DM_mortality_survplot_DEPANXr, width = 5, height = 4)
+
+
+T2DM_mortality_survplot_PNC <- ggsurvplot(fit = survfit(Surv(time = time_to_event_weeks, event = status) ~ PNC,
+                                                            data = model_data_mortality), 
+                                              xlab = 'Weeks', ylab = 'Cumulative incidence',
+                                              censor = FALSE,  fun = 'event', ggtheme = theme_bw(), size = 0.5,
+                                              palette = THF_discrete_colour_order[1:2], legend = c(0.2,0.85),
+                                              legend.labs = c('Without PNC', 'With PNC'), title = 'All-cause mortality') 
+
+T2DM_mortality_survplot_PNC <- T2DM_mortality_survplot_PNC$plot +
+  theme(legend.title = element_blank())
+
+ggsave(str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_Mortality_PNC.png'), 
+       T2DM_mortality_survplot_PNC, width = 5, height = 4)
+
+T2D_mortality_natrisk_DEPANXr <- surv_summary(survfit(Surv(time = time_to_event_weeks, event = status) ~ DEPANXr,
+                                              data = model_data_mortality), data = model_data_mortality)
+
+write_csv(T2D_mortality_natrisk_DEPANXr, str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_Mortality_DEPANXr.csv'))
+
+T2D_mortality_natrisk_PNC <- surv_summary(survfit(Surv(time = time_to_event_weeks, event = status) ~ PNC,
+                                              data = model_data_mortality), data = model_data_mortality)
+
+write_csv(T2D_mortality_natrisk_PNC, str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_Mortality_PNC.csv'))
+
+
+# Prepare T2D modelling data for emergency hospital admissions ----
 
 em_admissions_study <- hesapc_admissions %>%
   filter(epistart %within% interval(study_start, study_end) & admitype == 'emergency') %>% 
@@ -149,32 +181,14 @@ em_admissions_study <- hesapc_admissions %>%
   ungroup()
 
 
-model_data_adm <- patients %>% 
-  filter(cohort_step2 == 1) %>% 
-  select(patid, gender, pracid, 
-         startage_study, age_bins_study, age_bins_study_SDC, 
-         imd_quintile, diabetes_type, medication, smoking_status, BMI_categorical, HbA1C_control,
-         time_since_diagnosis, 
-         mm_cat, mm_count, physical_mm_count, physical_mm_cat, mental_mm_cat_SDC, DEPANX, DEPANXr, SCZ, PNC,
-         ONS_dod)  %>% 
-  mutate(mental_disorder = 'None',
-         mental_disorder = ifelse(DEPANXr == 1, 'CMD', mental_disorder),
-         mental_disorder = ifelse(SCZ == 1, 'SMD', mental_disorder),
-         DEPANXr_PNC = case_when(DEPANXr == 0 & PNC == 0 ~ 'none',
-                                 DEPANXr == 1 & PNC == 0 ~ 'DEPANXr',
-                                 DEPANXr == 0 & PNC == 1 ~ 'PNC',
-                                 DEPANXr == 1 & PNC == 1 ~ 'DEPANXr/PNC'),
-         DEPANXr_PNC = fct_relevel(DEPANXr_PNC, 'none', 'DEPANXr', 'PNC', 'DEPANXr/PNC')) %>% 
-  left_join(em_admissions_study[, c('patid', 'epistart')], by = 'patid') 
-
-         
 # Censoring time 
-
 # Type of even/censoring
 # 1 = censored (study end, or death)
 # 2 = admitted
 
-model_data_adm <-  model_data_adm %>% 
+model_data_adm <- patients %>% 
+  filter(cohort_step2 == 1 & diabetes_type =='type2') %>% 
+  left_join(em_admissions_study[, c('patid', 'epistart')], by = 'patid') %>% 
   group_by(patid) %>% 
   mutate(earliest_eventdate = min(ONS_dod, epistart, study_end, na.rm = TRUE),
          time_to_event = as.numeric(earliest_eventdate - study_start, 'days'),
@@ -184,48 +198,89 @@ model_data_adm <-  model_data_adm %>%
   ungroup()
 
 
-View(model_data_adm %>% select(patid, ONS_dod, epistart, earliest_eventdate, time_to_event, status))
-
 model_data_adm <-  model_data_adm %>% 
   mutate(time_to_event_weeks = ceiling(time_to_event/7),
-         time_to_event_weeks = ifelse(time_to_event_weeks == 0, 1, time_to_event_weeks))
-
-model_data_adm <-  model_data_adm %>% 
-  filter(diabetes_type  == 'type2') %>% 
-  mutate(diabetes_type = fct_drop(diabetes_type),
+         time_to_event_weeks = ifelse(time_to_event_weeks == 0, 1, time_to_event_weeks),
+         diabetes_type = fct_drop(diabetes_type),
          female = ifelse(gender == '1', 0, 1),
          female = factor(female),
          imd_quintile_numeric = as.numeric(gsub(' .*', '', imd_quintile)),
          smoker = ifelse(smoking_status == 'smoker', 1,0),
          newly_diagnosed_1y = ifelse(time_since_diagnosis == 'less than 1 year', 1, 0),
-         HbA1C_control_bad = ifelse(HbA1C_control == 'bad', 1, 0))
-
-model_data_adm_T2D <- model_data_adm %>% 
-  mutate(age_centered = startage_study - round(mean(startage_study)),
-         age_5ybin_centered = age_centered / 5)
+         age_centered = startage_study - round(mean(startage_study)),
+         age_5ybin_centered = age_centered / 5,
+         mm_count_excl_DEPANXr = rowSums(.[comorbidities[comorbidities != 'DEPANXr']]),
+         mm_count_excl_PNC = rowSums(.[comorbidities[comorbidities != 'PNC']]))
 
 
 # Cox models for T2D
-T2D_adm_coxmodel_1 <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric + DEPANXr 
-                            + HbA1C_control_bad + physical_mm_count , data = model_data_adm_T2D)
-summary(T2D_adm_coxmodel_1)
-extractAIC(T2D_adm_coxmodel_1)
+T2D_adm_coxmodel_base <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric  
+                               + HbA1C_control + smoker + newly_diagnosed_1y, data = model_data_adm)
+summary(T2D_adm_coxmodel_base)
+extractAIC(T2D_adm_coxmodel_base)
+T2D_adm_coxmodel_base_summary <- summariseCoxModel(model = T2D_adm_coxmodel_base)
+write_csv(T2D_adm_coxmodel_base_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_EmAdmissions_basemodel.csv'))
 
-T2D_adm_coxmodel_2 <- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric + DEPANXr 
-                            + HbA1C_control + physical_mm_count + smoker, data = model_data_adm_T2D) 
-summary(T2D_adm_coxmodel_2)
-extractAIC(T2D_adm_coxmodel_2)
+# DEPANXr
+T2D_adm_coxmodel_DEPANXr <- update(T2D_adm_coxmodel_base, . ~ . + mm_count_excl_DEPANXr + DEPANXr )
+summary(T2D_adm_coxmodel_DEPANXr)
+extractAIC(T2D_adm_coxmodel_DEPANXr)
 
-T2D_adm_coxmodel_2_summary <- summariseCoxModel(model = T2D_adm_coxmodel_2)
-write_csv(T2D_adm_coxmodel_2_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_EmergencyAdmission_DEPANXr.csv'))
+T2D_adm_coxmodel_DEPANXr_summary <- summariseCoxModel(model = T2D_adm_coxmodel_DEPANXr)
+write_csv(T2D_adm_coxmodel_DEPANXr_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_EmergencyAdmissions_DEPANXr.csv'))
+
+# PNC
+T2D_adm_coxmodel_PNC <- update(T2D_adm_coxmodel_base, . ~ . + mm_count_excl_PNC + PNC )
+summary(T2D_adm_coxmodel_PNC)
+extractAIC(T2D_adm_coxmodel_PNC)
+
+T2D_adm_coxmodel_PNC_summary <- summariseCoxModel(model = T2D_adm_coxmodel_PNC)
+write_csv(T2D_adm_coxmodel_PNC_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_EmergencyAdmissions_PNC.csv'))
+
+# Model all conditions invididually 
+
+T2D_adm_coxmodel_allmm <-  update(T2D_adm_coxmodel_base, as.formula(str_c('. ~ . + ', str_c(comorbidities, collapse = ' + '))))
+
+summary(T2D_adm_coxmodel_allmm)
+extractAIC(T2D_adm_coxmodel_allmm)
+T2D_adm_coxmodel_allmm_summary <- summariseCoxModel(model = T2D_adm_coxmodel_allmm)
+write_csv(T2D_adm_coxmodel_allmm_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_EmergencyAdmissions_allmm.csv'))
+
+# Kaplan meier plots
+
+T2DM_adm_survplot_DEPANXr <- ggsurvplot(fit = survfit(Surv(time = time_to_event_weeks, event = status) ~ DEPANXr,
+                                                            data = model_data_adm), 
+                                              xlab = 'Weeks', ylab = 'Cumulative incidence',
+                                              censor = FALSE,  fun = 'event', ggtheme = theme_bw(), size = 0.5,
+                                              palette = THF_discrete_colour_order[1:2], legend = c(0.2,0.85),
+                                              legend.labs = c('Without CMD', 'With CMD'), title = 'Emergency Hospital Admissions') 
+
+T2DM_adm_survplot_DEPANXr <- T2DM_adm_survplot_DEPANXr$plot +
+  theme(legend.title = element_blank())
+
+ggsave(str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_EmergencyAdmissions_DEPANXr.png'), 
+       T2DM_adm_survplot_DEPANXr, width = 5, height = 4)
 
 
-T2D_adm_coxmodel_4<- coxph(Surv(time = time_to_event, event = status) ~ female + age_5ybin_centered + imd_quintile_numeric + DEPANXr_PNC 
-                           + HbA1C_control + physical_mm_count + smoker, data = model_data_adm_T2D) # best
+T2DM_adm_survplot_PNC <- ggsurvplot(fit = survfit(Surv(time = time_to_event_weeks, event = status) ~ PNC,
+                                                        data = model_data_adm), 
+                                          xlab = 'Weeks', ylab = 'Cumulative incidence',
+                                          censor = FALSE,  fun = 'event', ggtheme = theme_bw(), size = 0.5,
+                                          palette = THF_discrete_colour_order[1:2], legend = c(0.2,0.85),
+                                          legend.labs = c('Without PNC', 'With PNC'), title = 'Emergency Hospital Admissions') 
 
-summary(T2D_adm_coxmodel_4)
-extractAIC(T2D_adm_coxmodel_4)
-broom::tidy(T2D_adm_coxmodel_4, exponentiate = TRUE)
-T2D_adm_coxmodel_4_summary <- summariseCoxModel(model = T2D_adm_coxmodel_4)
-write_csv(T2D_adm_coxmodel_4_summary, str_c(summary_stats_path, 'outcomes_modelling/Coefficients_T2D_EmergencyAdmission_DEPANXr_PNC.csv'))
+T2DM_adm_survplot_PNC <- T2DM_adm_survplot_PNC$plot +
+  theme(legend.title = element_blank())
 
+ggsave(str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_EmergencyAdmissions_PNC.png'), 
+       T2DM_adm_survplot_PNC, width = 5, height = 4)
+
+T2D_adm_natrisk_DEPANXr <- surv_summary(survfit(Surv(time = time_to_event_weeks, event = status) ~ DEPANXr,
+                                                      data = model_data_adm), data = model_data_adm)
+
+write_csv(T2D_mortality_natrisk_DEPANXr, str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_EmergencyAdmissions_DEPANXr.csv'))
+
+T2D_adm_natrisk_PNC <- surv_summary(survfit(Surv(time = time_to_event_weeks, event = status) ~ PNC,
+                                                  data = model_data_adm), data = model_data_adm)
+
+write_csv(T2D_adm_natrisk_PNC, str_c(summary_stats_path, 'outcomes_modelling/Summary_T2D_EmergencyAdmissions_PNC.csv'))
