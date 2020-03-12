298673e1c1c82d1aca97d7dfa5ccf0a9839b0c63
fiona-grimm <fiona.grimm.zh@gmail.com>
2020-03-12 16:39:43 +0000

    Implement separate models for CMD and PNC.

--- R/17_utilisation_modelling.R
+++ R/17_utilisation_modelling.R
@@ -108,27 +108,39 @@ AICtab(m_T2D_primcare_1a, m_T2D_primcare_1b)
 m_T2D_primcare_2 <- update(m_T2D_primcare_1b, . ~ . + HbA1C_control + smoker + newly_diagnosed_1y )
 summary(m_T2D_primcare_2)
 
-# Add multimorbidity: count of physical conditions and depression/anxiety as binary
-m_T2D_primcare_3 <- update(m_T2D_primcare_2, . ~ . + physical_mm_count + DEPANXr )
-summary(m_T2D_primcare_3)
+# CMD: Add multimorbidity (count of physical conditions) and depression/anxiety as binary
+m_T2D_primcare_3_DEPANXr <- update(m_T2D_primcare_2, . ~ . + mm_count_excl_DEPANXr + DEPANXr )
+summary(m_T2D_primcare_3_DEPANXr)
 
-AICtab(m_T2D_primcare_base, m_T2D_primcare_1b, m_T2D_primcare_2, m_T2D_primcare_3) # full model is the best model
-stepAIC(m_T2D_primcare_3, direction = 'backward') # none eliminated
+AICtab(m_T2D_primcare_base, m_T2D_primcare_1b, m_T2D_primcare_2, m_T2D_primcare_3_DEPANXr) # full model is the best model
+stepAIC(m_T2D_primcare_3_DEPANXr, direction = 'backward') # none eliminated
 
-summariseNbModel(model = m_T2D_primcare_3) %>% 
-  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Primarycare.csv'))
+summariseNbModel(model = m_T2D_primcare_3_DEPANXr) %>% 
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Primarycare_DEPANXr.csv'))
 
-# Model physical conditions invididually (top 20 according to prevalence)
-m_T2D_primcare_4 <- update(m_T2D_primcare_2, as.formula(str_c('. ~ . + ', str_c(mm_prev_T2D$condition, collapse = ' + '))))
+# PNC: Add multimorbidity (count of physical conditions) and painful condition as binary
+m_T2D_primcare_3_PNC <- update(m_T2D_primcare_2, . ~ . + mm_count_excl_PNC + PNC )
+summary(m_T2D_primcare_3_PNC)
+
+AICtab(m_T2D_primcare_base, m_T2D_primcare_1b, m_T2D_primcare_2, m_T2D_primcare_3_PNC) # full model is the best model
+stepAIC(m_T2D_primcare_3_PNC, direction = 'backward') # none eliminated
+
+summariseNbModel(model = m_T2D_primcare_3_PNC) %>% 
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Primarycare_PNC.csv'))
+
+# Model physical conditions invididually 
+m_T2D_primcare_4 <- update(m_T2D_primcare_2, as.formula(str_c('. ~ . + ', str_c(comorbidities, collapse = ' + '))))
 summary(m_T2D_primcare_4)
 
-AICtab(m_T2D_primcare_3, m_T2D_primcare_4) # model with individual conditions is better
+AICtab(m_T2D_primcare_3_DEPANXr, m_T2D_primcare_3_PNC , m_T2D_primcare_4) # model with individual conditions is better
 stepAIC(m_T2D_primcare_4, direction = 'backward') # eliminating smoking - minimal improvement
 
 summariseNbModel(model = m_T2D_primcare_4) %>% 
-  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Primarycare_20mm.csv'))
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Primarycare_allmm.csv'))
 
-## Outpatient attendances
+
+# Outpatient attendances --------------------------------------------------
+
 # Base model
 m_T2D_OPattends_base <- glm.nb(OP_attended_total ~ female + age_5ybin_centered + offset(log(years_in_study_cprd)), 
                               data = model_data_T2D)
@@ -148,41 +160,66 @@ AICtab(m_T2D_OPattends_1a, m_T2D_OPattends_1b)
 m_T2D_OPattends_2 <- update(m_T2D_OPattends_1b, . ~ . + HbA1C_control + smoker + newly_diagnosed_1y )
 summary(m_T2D_OPattends_2)
 
-# Add multimorbidity: count of physical conditions and depression/anxiety as binary
-m_T2D_OPattends_3 <- update(m_T2D_OPattends_2, . ~ . + physical_mm_count + DEPANXr )
-summary(m_T2D_OPattends_3)
+# CMD: Add multimorbidity (count of physical conditions) and depression/anxiety as binary
+m_T2D_OPattends_3_DEPANXr  <- update(m_T2D_OPattends_2, . ~ . + mm_count_excl_DEPANXr + DEPANXr )
+summary(m_T2D_OPattends_3_DEPANXr)
 
-AICtab(m_T2D_OPattends_base, m_T2D_OPattends_1b, m_T2D_OPattends_2, m_T2D_OPattends_3) # full model is the best model
-stepAIC(m_T2D_OPattends_3, direction = 'backward') # none eliminated
+AICtab(m_T2D_OPattends_base, m_T2D_OPattends_1b, m_T2D_OPattends_2, m_T2D_OPattends_3_DEPANXr) # full model is the best model
+stepAIC(m_T2D_OPattends_3_DEPANXr, direction = 'backward') # none eliminated
 
-summariseNbModel(model = m_T2D_OPattends_3) %>% 
-  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Outpatients.csv'))
+summariseNbModel(model = m_T2D_OPattends_3_DEPANXr) %>% 
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Outpatients_DEPANXr.csv'))
 
-# Model physical conditions invididually (top 20 according to prevalence)
-m_T2D_OPattends_4 <- update(m_T2D_OPattends_2, as.formula(str_c('. ~ . + ', str_c(mm_prev_T2D$condition, collapse = ' + '))))
+
+# PNC: Add multimorbidity (count of physical conditions) and painful condition as binary
+m_T2D_OPattends_3_PNC <- update(m_T2D_OPattends_2, . ~ . + mm_count_excl_PNC + PNC )
+summary(m_T2D_OPattends_3_PNC)
+
+AICtab(m_T2D_OPattends_base, m_T2D_OPattends_1b, m_T2D_OPattends_2, m_T2D_OPattends_3_PNC) # full model is the best model
+stepAIC(m_T2D_OPattends_3_PNC, direction = 'backward') # none eliminated
+
+summariseNbModel(model = m_T2D_OPattends_3_PNC) %>% 
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Outpatients_PNC.csv'))
+
+
+# Model physical conditions invididually 
+m_T2D_OPattends_4 <- update(m_T2D_OPattends_2, as.formula(str_c('. ~ . + ', str_c(comorbidities, collapse = ' + '))))
 summary(m_T2D_OPattends_4)
 
-AICtab(m_T2D_OPattends_3, m_T2D_OPattends_4) # model with individual conditions is better
+AICtab(m_T2D_OPattends_3_DEPANXr, m_T2D_OPattends_3_PNC, m_T2D_OPattends_4) # model with individual conditions is better
 stepAIC(m_T2D_OPattends_4, direction = 'backward') # eliminating smoking - minimal improvement
 
 summariseNbModel(model = m_T2D_OPattends_4) %>% 
-  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Outpatients_20mm.csv'))
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_Outpatients_allmm.csv'))
 
-# A&E attendances
-m_T2D_AE_1 <- glm.nb(AE_attendances ~ female + age_5ybin_centered + offset(log(years_in_study_cprd))
-                     + imd_quintile_numeric + HbA1C_control + smoker + newly_diagnosed_1y + physical_mm_count + DEPANXr, 
+
+# A&E attendances ---------------------------------------------------------
+
+# CMD: Add multimorbidity (count of physical conditions) and depression/anxiety as binary
+m_T2D_AE_1_DEPANXr <- glm.nb(AE_attendances ~ female + age_5ybin_centered + offset(log(years_in_study_cprd))
+                     + imd_quintile_numeric + HbA1C_control + smoker + newly_diagnosed_1y + mm_count_excl_DEPANXr + DEPANXr, 
                      data = model_data_T2D)
 
-summary(m_T2D_AE_1)
-summariseNbModel(model = m_T2D_AE_1) %>% 
-  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_A&E.csv'))
+summary(m_T2D_AE_1_DEPANXr)
+summariseNbModel(model = m_T2D_AE_1_DEPANXr) %>% 
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_A&E_DEPANXr.csv'))
+
+# PNC: Add multimorbidity (count of physical conditions) and painful condition as binary
+m_T2D_AE_1_PNC <- glm.nb(AE_attendances ~ female + age_5ybin_centered + offset(log(years_in_study_cprd))
+                         + imd_quintile_numeric + HbA1C_control + smoker + newly_diagnosed_1y + mm_count_excl_PNC + PNC, 
+                         data = model_data_T2D)
+
+summary(m_T2D_AE_1_PNC)
+summariseNbModel(model = m_T2D_AE_1_PNC) %>% 
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_A&E_PNC.csv'))
+
 
 m_T2D_AE_2 <- glm.nb(as.formula(str_c('AE_attendances ~ female + age_5ybin_centered + offset(log(years_in_study_cprd)) + imd_quintile_numeric + HbA1C_control + smoker + newly_diagnosed_1y + ', 
-                                      str_c(mm_prev_T2D$condition, collapse = ' + '))), 
+                                      str_c(comorbidities, collapse = ' + '))), 
                      data = model_data_T2D)
 
 summary(m_T2D_AE_2)
 summariseNbModel(model = m_T2D_AE_2) %>% 
-  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_A&E_20mm.csv'))
+  write_csv(str_c(summary_stats_path, 'utilisation_modelling/Coefficients_T2D_A&E_allmm.csv'))
 
-AICtab(m_T2D_AE_1, m_T2D_AE_2) # model with individual conditions is better
+AICtab(m_T2D_AE_1_DEPANXr, m_T2D_AE_1_PNC, m_T2D_AE_2) # model with individual conditions is better
